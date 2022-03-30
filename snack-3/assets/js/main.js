61323d059ec40e9fc4095e1dd67d693c9eb6bb9d
robertolatini31 <robertolatini31@gmail.com>
2022-03-30 11:40:21 +0200

    added snack-3

---
+++ snack-3/assets/js/main.js
@@ -0,0 +1,20 @@
+// SNACK 3
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+
+// creo una variabile di somma e la imposto a 0
+let total = 0;
+
+// creo un ciclo per 10 iterazioni
+for (let i = 0; i < 10; i++) {
+    // chiedo un numero all'utente
+    let user_number = parseInt(prompt('inserisci un numero'));
+    console.log(`inserito: ${user_number}`);
+    total = total + user_number;
+    // console.log(`somma parziale: ${total}`);
+}
+
+// stampo la somma
+console.log(`somma finale: ${total}`);
+    
+
