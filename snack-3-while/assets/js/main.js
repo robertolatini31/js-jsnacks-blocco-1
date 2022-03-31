ee6b1a3ced6f0e1bff887ab3772158cf0bb55960
robertolatini31 <robertolatini31@gmail.com>
2022-03-31 11:40:20 +0200

    added snack-3 while

---
+++ snack-3-while/assets/js/main.js
@@ -0,0 +1,27 @@
+// SNACK 3
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+
+// creo una variabile di somma e la imposto a 0
+let total = 0;
+let i = 0
+// creo un ciclo per 10 iterazioni
+while (i < 10) {
+    // chiedo un numero all'utente
+    let user_number = parseInt(prompt('inserisci un numero'));
+    console.log(`inserito: ${user_number}`);
+    total = total + user_number;
+    // console.log(`somma parziale: ${total}`);
+    i++
+}
+
+// stampo la somma
+// console.log(`somma finale: ${total}`);
+    
+
+if (isNaN(total)) {
+    console.log(`errore inserito: ${total}`);
+ } else {
+    console.log(`somma finale: ${total}`);
+    
+ }
